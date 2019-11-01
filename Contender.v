4e7a5bd329c140ac475b554492d875c21ede8e7a
Cody Roux <cody.roux@gmail.com>
2019-11-01 11:08:32 -0400

    Getting the ball rolling!

---
+++ Contender.v
@@ -0,0 +1,16 @@
+Require Import Arith Lia.
+
+
+(* This is the previous contender for largest number. *)
+(* Submitted by codyroux *)
+Definition contender_0 : nat := 0.
+
+(* This is the current contender for largest number. *)
+(* Submitted by codyroux *)
+Definition contender_1 : nat := 42.
+
+Theorem contender_0_lt_contender_1 : contender_0 < contender_1.
+Proof.
+  unfold contender_0, contender_1.
+  auto with arith.
+Qed.
---
+++ Graveyard.v
@@ -0,0 +1,11 @@
+Require Import Arith Lia.
+
+
+(* This is the current contender for largest number. *)
+(* Submitted by codyroux *)
+Definition contender_0 : nat := 0.
+
+Theorem contender_0_lt_nothing : True.
+Proof.
+  auto.
+Qed.
