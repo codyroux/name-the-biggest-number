ee8afd2f2e4b41c71be789fd89ef2e3eda41826a
Jay Kruer <j@dank.systems>
2019-11-01 23:05:09 -0400

    Add contender_2 and proof

--- Graveyard.v
+++ Graveyard.v
@@ -9,3 +9,12 @@ Theorem contender_0_lt_nothing : True.
 Proof.
   auto.
 Qed.
+
+(* Submitted by codyroux *)
+Definition contender_1 : nat := 42.
+
+Theorem contender_0_lt_contender_1 : contender_0 < contender_1.
+Proof.
+  unfold contender_0, contender_1.
+  auto with arith.
+Qed.
